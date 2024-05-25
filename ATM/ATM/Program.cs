0419e513f3883e5592c91bfefebd5e01b9d3ef0f
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2024-05-25 12:02:30 +0100

    grit

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System; 
+
+namespace MyProject;
+class cardHolder
+{
+ static void Main(string[] args)
+ {
+  //Console.WriteLine("Hello, World!");
+  string cardNum;
+  int pin;
+  string firstName;
+  string lastName;
+  double balance;
+
+  public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+  {
+   this.cardNum = cardNum;
+   this.pin = pin;
+   this.firstName = firstName;
+   this.lastName = lastName;
+   this.balance = balance;
+  }
+
+  public string getNum()
+  {
+   return cardNum;
+  }
+
+  public int getPin()
+  {
+   return pin;
+  }
+  public string firstName()
+  {
+   return firstName;
+  }
+  public string lastName()
+  {
+   return lastName;
+  }
+  public double getBalance()
+  {
+   return balance; 
+  }
+
+  public void setNum(string newCardNum)
+  {
+   cardNum = newCardNum;
+  }
+  public void setPin(double newPin)
+  {
+   pin = newPin;
+  }
+
+ }
+}
