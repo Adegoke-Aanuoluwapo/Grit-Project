9b56563533a5d04e2d7ce5968333b7ae3e1253b7
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2024-05-27 22:35:50 +0100

    gratitude

--- Quiz Game/Program.cs
+++ Quiz Game/Program.cs
@@ -1,3 +1,16 @@
 ﻿using System.IO;
 
 Console.WriteLine("Hello");
+string filePath = "questions.txt";
+// string[] text = File.ReadAllLines("questions.txt");
+if (File.Exists(filePath))
+{
+
+ string[] text = File.ReadAllLines("questions.txt");
+foreach(string line in text){
+ Console.WriteLine(line);
+}
+}
+else{
+ Console.WriteLine($"Error: File not found at path {filePath}");
+}
--- Quiz Game/obj/Debug/net8.0/Quiz Game.AssemblyInfo.cs
+++ Quiz Game/obj/Debug/net8.0/Quiz Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4105239b82689c1e554f830d4366d4d788c9e317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5acac292ee1d455fdb96e02eeb60ddd5b56d2026")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
