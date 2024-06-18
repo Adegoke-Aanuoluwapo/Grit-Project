f19a294614f6cf1970cff16b0d397ddf31e6180e
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2024-06-18 12:26:33 +0100

    fizz buzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace Tutorial
+{   
+  public static class Program{
+
+   
+           static void Main(string[] args)
+           {
+          
+              
+              Console.WriteLine("Enter a number: ");
+              int number = Convert.ToInt32(Console.ReadLine());
+            
+               
+              if(number % 3 == 0 && number % 5 == 0)
+              {
+                Console.WriteLine("The number is FizzBuzz");
+              }
+            
+              else if(number % 5 == 0)
+              {
+                Console.WriteLine("The number is Buzz");
+
+              }
+
+              else if(number % 3 == 0)
+              
+              {
+                Console.WriteLine("The number is Fizz");
+              }
+             
+           
+
+
+    }
+    
+  
+  }
+}
