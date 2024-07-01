45478436a2850e8461128ea56efb7eddd48d6b7b
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2024-07-01 14:01:49 +0100

    more

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //     Console.WriteLine(Add(10, 50));
+          
+        //     int num = ReadInt("Enter a number ");
+            
+        //     Console.WriteLine(num);
+
+        //     int firstNum = ReadInt("Enter first number:");
+        //     int secondNum = ReadInt("Enter second number:");
+        //     int sum = Add(firstNum, secondNum);
+        //     int product = Multiply(firstNum, secondNum);
+            
+        //     Console.WriteLine(sum);
+        //     Console.WriteLine();
+        //     Console.WriteLine(product);
+          
+           
+        //     Console.WriteLine();
+        // }
+        // static string UserDetails(string name, int age)
+        // {
+        //     return $"Hello my name is: {name}, and I am {age}years old";
+        // }
+        // static int ReadInt(string message)
+        // {
+        //       Console.WriteLine($"{message}");
+        //    int num1 = Convert.ToInt32(Console.ReadLine());
+        //    return num1;
+        // }
+        // static int Add(int a, int b)
+        // {
+        //     return a + b;
+        // }
+        //  static int Multiply(int a, int b)
+        // {
+        //     return a * b;
+        // }
+        }
+    }
+}
--- Functions/Functions/obj/Debug/net8.0/Functions.AssemblyInfo.cs
+++ Functions/Functions/obj/Debug/net8.0/Functions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c4a159c35ad1c18a799ede2c25a1d16d365ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4989438187912f7ee0fe1a3bd64c7dce2ee417")]
 [assembly: System.Reflection.AssemblyProductAttribute("Functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
